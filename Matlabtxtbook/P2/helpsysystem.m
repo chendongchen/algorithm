9764ee96a8603b2364669cc3b2b58e5e1ca01214
chendongchen <3242986122@qq.com>
2023-08-21 01:00:04 +0800

    2023.8.21

---
+++ Matlabtxtbook/P2/helpsysystem.m
@@ -0,0 +1,13 @@
+%2023/8/21 陈东
+%Matlab 内容查找及帮助系统
+%path 可以用于添加搜索目录
+%help 是帮助系统的核心
+
+%help 会显示所有的项目文件
+help 
+
+%help + function name 会显示函数
+help elg
+
+%lookfor + function name 可以进行模糊查找，关键字。
+lookfor quadratic
